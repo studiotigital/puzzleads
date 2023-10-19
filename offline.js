1844343ed5773e6c63abd93f9d616fbd403d076b
studiotigital <56325411+studiotigital@users.noreply.github.com>
2023-10-19 18:14:52 -0300

    Add files via upload
    Signed-off-by: studiotigital <56325411+studiotigital@users.noreply.github.com>

---
+++ offline.js
@@ -0,0 +1,21 @@
+﻿{
+	"version": 1697749899,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/peco_destino-sheet0.png",
+		"images/peco_destino-sheet1.png",
+		"images/particles.png",
+		"images/sprite-sheet0.png",
+		"images/sprite2-sheet0.png",
+		"images/play-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
